49b58a81614d576cf747ef75359ec53441510ddf
alex <alexdeineko830@gmail.com>
2024-04-16 02:50:53 +0300

    Part 5 - 5

--- service/services/models.py
+++ service/services/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
+from services.tasks import set_price
 
 from clients.models import Client
 
@@ -7,6 +8,18 @@ from clients.models import Client
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -21,7 +34,21 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
+
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
